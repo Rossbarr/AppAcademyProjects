9195568315cb6e6412f5dc200441cc2e4c097aad
rossbarr <rossbarr@msu.edu>
2020-11-26 15:25:01 -0500

    Completing error handling funtime

---
+++ ruby/error_handling_funtime/super_useful.rb
@@ -0,0 +1,55 @@
+# PHASE 2
+def convert_to_int(str)
+  Integer(str)
+rescue ArgumentError
+  return nil
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise ArgumentError
+  else 
+    raise StandardError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit)
+rescue ArgumentError
+  puts "I don't want this, but since it's coffee..."
+  retry
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("Years known must be greater than 5 in order to be best friends") if yrs_known < 5
+    raise ArgumentError.new("Need name and favorite pastime") if name.length <= 0 or fav_pastime.length <= 0
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+

143f223ebb62e075e838b06f013538e4cad67a65
rossbarr <rossbarr@msu.edu>
2020-11-29 23:14:11 -0500

    Finishing simon, pointless project

--- ruby/simon/lib/simon.rb
+++ ruby/simon/lib/simon.rb
@@ -4,38 +4,58 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
   end
 
   def play
-
+    while @game_over == false
+      take_turn
+    end
+    if @game_over
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    system("clear")
+    show_sequence
+    if @seq != require_sequence
+      @game_over = true
+    else
+      @sequence_length += 1
+      round_success_message
+    end
   end
 
   def show_sequence
-
+    color = add_random_color
+    puts(color)
   end
 
   def require_sequence
-
+    gets.chomp.split(" ")
   end
 
   def add_random_color
-
+    color = COLORS.sample()
+    @seq << color
+    return color
   end
 
   def round_success_message
-
+    puts "Success!"
   end
 
   def game_over_message
-
+    puts "Failure!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
   end
 end

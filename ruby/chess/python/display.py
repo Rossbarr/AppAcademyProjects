5e3d30fac97b931c23341f76606e9792c948a65e
rossbarr <rossbarr@msu.edu>
2020-11-30 21:54:49 -0500

    update display.py

--- ruby/chess/python/display.py
+++ ruby/chess/python/display.py
@@ -2,4 +2,15 @@ import tkinter as tk
 
 window = tk.Tk()
 
+for i in range(8):
+    for j in range(8):
+        button = tk.Button(
+                master = window,
+                relief = tk.RAISED,
+                borderwidth = 1
+                )
+        button.grid(row = i, column = j, padx = 5, pady = 5)
+        label = tk.Label(master = button, text = f"Row {i}\nColumn {j}")
+        label.pack()
+
 window.mainloop()

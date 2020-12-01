e6606ed255b5943199f4de1a6d079775192a6c90
rossbarr <rossbarr@msu.edu>
2020-12-01 12:19:30 -0500

    Update display.py

--- ruby/chess/python/display.py
+++ ruby/chess/python/display.py
@@ -1,16 +1,35 @@
 import tkinter as tk
 
-window = tk.Tk()
+class Display(tk.Frame):
+    def __init__(self, main):
+        self.main = main
+        main.title("Chessboard")
+        self.create_board()
 
-for i in range(8):
-    for j in range(8):
-        button = tk.Button(
-                master = window,
-                relief = tk.RAISED,
-                borderwidth = 1
-                )
-        button.grid(row = i, column = j, padx = 5, pady = 5)
-        label = tk.Label(master = button, text = f"Row {i}\nColumn {j}")
-        label.pack()
+    def create_board(self):
+        for i in range(8):
+            for j in range(8):
+                frame = tk.Frame(master = self.main, relief = tk.RAISED, borderwidth = 1)
+                frame.grid(row = i, column = j, padx = 5, pady = 5)
+                button = tk.Button(master = frame, text = f"Row {i}\nColumn {j}")
+                button["command"] = self.change_buttons
+                button.pack()
 
-window.mainloop()
+    def change_buttons(self):
+        pass
+        for i in range(8):
+            for j in range(8):
+                pass
+
+
+    def print_hello(self):
+        print("Hello")
+
+    def print_goodbye(self):
+        print("Goodbye")
+
+
+
+root = tk.Tk()
+chessboard = Display(root)
+root.mainloop()

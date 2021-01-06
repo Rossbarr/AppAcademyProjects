b8903a9f2546b45f88382fb362f11c8f528ccdb2
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-06 10:43:26 -0500

    Adding initial specs

--- ruby/poker/spec/player_spec.rb
+++ ruby/poker/spec/player_spec.rb
@@ -1,2 +1,25 @@
 require "rspec"
 require "player"
+
+describe "Player" do
+    subject(:player) { Player.new }
+    let(:hand) { Hand.new }
+
+    describe "#init" do
+        it "creates a hand variable" do
+            expect(player.hand).to be_a(Hand)
+        end
+
+        it "creates a pot variable" do
+            expect(player.pot).to be_a(Integer)
+        end
+    end
+
+    describe "#discard" do
+        it "expects user input" do
+            expect(player.input).to receive(:gets).with("1 2 4")
+        end
+    end
+end
+
+

40ccd290f47d0f7e20a2f87f58fcccec9b694c2d
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-06 16:16:07 -0500

    Finishing part 2 of the Octopus Problems project

--- ruby/octopus_problems/dancing_octopus.rb
+++ ruby/octopus_problems/dancing_octopus.rb
@@ -11,3 +11,16 @@ def slow_dance(move)
   end
   return -1
 end
+
+def fast_dance(move)
+  # I know creating the hash like this slows this method to O(n) time,
+  # but I'm too lazy to create the hash by hand, lol.
+  # At least I understand that hashes are O(1), right?
+  array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+  hash = {}
+  array.each_with_index do |movement, i|
+    hash[movement] = i
+  end
+
+  return hash[move]
+end

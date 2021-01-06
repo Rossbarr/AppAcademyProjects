16d2c877af3b51957c1e911a909b7829b9064773
rossbarr <rossbarr@msu.edu>
2021-01-05 23:32:23 -0500

    Finishing tests and lib for hand class

--- ruby/poker/lib/hand.rb
+++ ruby/poker/lib/hand.rb
@@ -1,5 +1,7 @@
 class Hand
     attr_reader :hand, :deck
+    attr_writer :hand
+
     def initialize(deck)
         @deck = deck
         @hand = []
@@ -9,10 +11,262 @@ class Hand
         if @hand.length < 5
             @hand << @deck.draw
         end
+
+        if @hand.length > 1
+            sort
+        end
     end
-    
-    def return(i)
+
+     def return(i)
         deck.return(@hand[i])
         @hand.delete(@hand[i])
+    end
+ 
+    def value
+        sort
+        if is_flush
+            if is_straight
+                return 100000 * @hand[0].index
+            else
+                return 1000 + @hand[0].index + @hand[1].index + @hand[2].index + @hand[3].index + @hand[4].index
+            end
+        elsif is_straight
+            return 500 + @hand[0].index
+        elsif is_four_of_a_kind
+            sort_four_of_a_kind
+            return 40000 + @hand[0].index
+        elsif is_full_house
+            sort_full_house
+            return 10000 + @hand[0].index + @hand[3].index
+        elsif is_three_of_a_kind
+            sort_three_of_a_kind
+            return 150 + @hand[0].index
+        elsif is_two_pair
+            sort_two_pair
+            return 100 + @hand[0].index + @hand[2].index
+        elsif is_two_of_a_kind
+            sort_two_of_a_kind
+            return 50 + @hand[0].index
+        else
+            return @hand[0].index
+        end
+    end
+
+    def size
+        return @hand.length
+    end
+
+    def sort(p = 2)
+        sorted = false
+        while not sorted
+            sorted = true
+            @hand.each_with_index do |card, i|
+                if i < @hand.length - 1 and card.index < @hand[i+1].index
+                    @hand[i], @hand[i+1] = @hand[i+1], @hand[i]
+                    sorted = false
+                end
+            end
+        end
+    end
+
+    private
+    
+    def is_flush
+        return @hand.all? { |card| card.suit == @hand[0].suit }
+    end
+
+    def is_straight
+        @hand.each_with_index do |card, i|
+            if i < 4
+                if not (@hand[i].index == @hand[i + 1].index + 1)
+                    return false
+                end
+            end
+        end
+        return true
+    end
+
+    def is_four_of_a_kind
+        if @hand.length >= 4
+            @hand.each_with_index do |card, i|
+                counter = 1
+                rank_counter = 1
+                while i + counter < @hand.length
+                    if @hand[i + counter].rank == card.rank
+                        rank_counter += 1
+                        if rank_counter == 4
+                            return true
+                        end
+                    end
+                    counter += 1
+                end
+            end
+        end
+        return false
+    end
+
+    def is_full_house
+        three_of_a_kind = false
+        three_of_a_kind_rank = nil
+        if @hand.length == 5
+            @hand.each_with_index do |card, i|
+                counter = 1
+                rank_counter = 1
+                while i + counter < @hand.length 
+                    if @hand[i + counter].rank == card.rank
+                        rank_counter += 1
+                        if rank_counter == 3
+                            three_of_a_kind = true
+                            three_of_a_kind_rank = card.rank
+                        end
+                    end
+                    counter += 1
+                end
+            end
+            @hand.each_with_index do |card, i|
+                counter = 1
+                if three_of_a_kind == true
+                    while i + counter < @hand.length
+                        if card.rank != three_of_a_kind_rank and @hand[i + counter].rank == card.rank
+                            return true
+                        end
+                        counter += 1
+                    end
+                end
+            end
+        end
+        return false
+    end
+   
+    def is_three_of_a_kind
+        if @hand.length >= 3
+            @hand.each_with_index do |card, i|
+                counter = 1
+                rank_counter = 1
+                while i + counter < @hand.length
+                    if @hand[i + counter].rank == card.rank
+                        rank_counter += 1
+                        if rank_counter == 3
+                                return true
+                        end
+                    end
+                    counter += 1
+                end
+            end
+        end
+        return false
+    end
+
+    def is_two_pair
+        pair_counter = 0
+        if @hand.length >= 4
+            @hand.each_with_index do |card, i|
+                counter = 1
+                while i + counter < @hand.length
+                    if @hand[i + counter].rank == card.rank
+                        pair_counter += 1
+                        if pair_counter == 2
+                            return true
+                        end
+                    end
+                    counter += 1
+                end
+            end
+        end
+        return false
+    end
+ 
+    def is_two_of_a_kind
+        if @hand.length >= 2
+            @hand.each_with_index do |card, i|
+                counter = 1
+                while i + counter < @hand.length
+                    if @hand[i + counter].rank == card.rank
+                        return true
+                    end
+                    counter += 1
+                end
+            end
+        end
+        return false
+    end
+
+    def sort_four_of_a_kind
+        if @hand[0].rank == @hand[1].rank and @hand[0].rank == @hand[2].rank
+            rank = @hand[0].rank
+        else
+            rank = @hand[1].rank
+        end
+        @hand.each_with_index do |card, i|
+            if card.rank != rank and i != 4
+                @hand[i], @hand[4] = @hand[4], @hand[i]
+            end
+        end
+    end
+
+    def sort_full_house
+        sort_three_of_a_kind
+    end
+    
+    def sort_three_of_a_kind
+        three_of_a_kind_rank = nil
+        @hand.each_with_index do |card, i|
+            if @hand[i + 1].rank == card.rank
+                three_of_a_kind_rank = card.rank
+                break
+            end
+        end
+        three_of_a_kind_array = []
+        rest_array = []
+        @hand.each do |card|
+            if card.rank == three_of_a_kind_rank
+                three_of_a_kind_array << card
+            else
+                rest_array << card
+            end
+        end
+        @hand = three_of_a_kind_array + rest_array
+    end
+
+    def sort_two_pair
+        first_pair = []
+        second_pair = []
+        other_card = []
+        if @hand[0].rank == @hand[1].rank
+            first_pair = [@hand[0], @hand[1]]
+        else
+            other_card = [@hand[0]]
+            first_pair = [@hand[1], @hand[2]]
+            second_pair = [@hand[3], @hand[4]]
+            @hand = first_pair + second_pair + other_card
+            return
+        end
+
+        if @hand[2] == @hand[3]
+            second_pair = [@hand[2], @hand[3]]
+            other_card = [@hand[4]]
+            @hand = first_pair + second_pair + other_card
+            return
+        else
+            other_card = [@hand[2]]
+            second_pair = [@hand[3], @hand[4]]
+            @hand = first_pair + second_pair + other_card
+            return
+        end
+    end
+
+    def sort_two_of_a_kind
+        pair_array = []
+        rest_array = []
+        @hand.each_with_index do |card, i|
+            counter = 1
+            while i + counter < @hand.length
+                if @hand[i + counter].rank == card.rank
+                    @hand[0], @hand[i] = @hand[i], @hand[0]
+                    @hand[1], @hand[i + counter] = @hand[i + counter], @hand[1]
+                end
+                counter += 1
+            end
+        end
     end
 end
--- ruby/poker/spec/hand_spec.rb
+++ ruby/poker/spec/hand_spec.rb
@@ -2,6 +2,8 @@ require "rspec"
 require "hand"
 require "deck"
 
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+
 describe "Hand" do
     let(:deck) { Deck.new }
     subject(:hand) { Hand.new(deck) }
@@ -41,8 +43,153 @@ describe "Hand" do
     end
 
     describe "#value" do
-        it "returns the value of its hand" do
-            expect(2).to eq(3)
+        context "when the hand is a royal flush" do
+            it "returns a high value" do
+                card1 = Card.new("AH")
+                card2 = Card.new("KH")
+                card3 = Card.new("QH")
+                card4 = Card.new("JH")
+                card5 = Card.new("TH")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(100000*12)
+            end
+        end
+
+        context "when the hand is a straight flush" do
+            it "returns a 100000s value" do
+                card1 = Card.new("6H")
+                card2 = Card.new("5H")
+                card3 = Card.new("4H")
+                card4 = Card.new("3H")
+                card5 = Card.new("2H")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(100000*4)
+            end
+        end
+        
+        context "when the hand is a four-of-a-kind" do
+            it "returns a 40000s value" do
+                card1 = Card.new("7H")
+                card2 = Card.new("7S")
+                card3 = Card.new("7D")
+                card4 = Card.new("7C")
+                card5 = Card.new("TH")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(40000+5)
+            end
+        end
+
+        context "when the hand is a full house" do
+            it "returns a 10000s value" do
+                card1 = Card.new("JH")
+                card2 = Card.new("JS")
+                card3 = Card.new("JD")
+                card4 = Card.new("8H")
+                card5 = Card.new("8D")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(10000+9+6)
+            end
+        end
+
+        context "when the hand is a flush" do
+            it "returns a 1000s value" do
+                card1 = Card.new("AH")
+                card2 = Card.new("QH")
+                card3 = Card.new("TH")
+                card4 = Card.new("6H")
+                card5 = Card.new("2H")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(1000+12+10+8+4+0)
+            end
+        end
+
+        context "when the hand is a straight" do
+            it "returns a 500s value" do
+                card1 = Card.new("TH")
+                card2 = Card.new("9C")
+                card3 = Card.new("8D")
+                card4 = Card.new("7S")
+                card5 = Card.new("6H")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(500+8)
+            end
+        end
+
+        context "when the hand is a three-of-a-kind" do
+            it "returns a 150s value" do
+                card1 = Card.new("AH")
+                card2 = Card.new("AC")
+                card3 = Card.new("AS")
+                card4 = Card.new("TC")
+                card5 = Card.new("6D")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(150+12)
+            end
+        end
+
+        context "when the hand is a two pair" do
+            it "returns a 100s value" do
+                card1 = Card.new("7H")
+                card2 = Card.new("7S")
+                card3 = Card.new("4C")
+                card4 = Card.new("4H")
+                card5 = Card.new("TH")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(100+5+2)
+            end
+        end
+
+        context "when the hand is a one pair" do
+            it "returns a 50s value" do
+                card1 = Card.new("9H")
+                card2 = Card.new("9C")
+                card3 = Card.new("4D")
+                card4 = Card.new("3H")
+                card5 = Card.new("2S")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(50+7)
+            end
+        end
+
+        context "when the hand is a high card" do
+            it "returns a 1s value" do
+                card1 = Card.new("7H")
+                card2 = Card.new("5C")
+                card3 = Card.new("4H")
+                card4 = Card.new("3H")
+                card5 = Card.new("2H")
+                hand.hand = [card1, card2, card3, card4, card5]
+                expect(hand.value).to be_a(Integer)
+                expect(hand.value).to eq(5)
+            end
+        end
+    end
+
+    describe "#sort" do
+        it "sorts the hand from highest value card to lowest value card" do
+            card1 = Card.new("7H")
+            card2 = Card.new("5C")
+            card3 = Card.new("4H")
+            card4 = Card.new("3H")
+            card5 = Card.new("2H")
+            hand.hand = [card2, card4, card5, card1, card3]
+            hand.sort
+            RANKS.index(hand.hand[4].rank) < RANKS.index(hand.hand[0].rank)
+        end
+    end
+
+    describe "#size" do
+        it "returns the number of cards" do
+            expect(hand.size).to eq(hand.hand.length)
         end
     end
 end

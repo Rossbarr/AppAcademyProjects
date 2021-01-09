3cd41f7e8afce1d1c25a4eee389e76ab459e6fee
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-09 12:01:31 -0500

    Complete phase 4

--- ruby/execution_time_differences_algorithms/project_4.rb
+++ ruby/execution_time_differences_algorithms/project_4.rb
@@ -15,3 +15,84 @@ puts windowed_max_range([1, 0, 2, 5, 4, 8], 2) # 4, 8
 puts windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
 puts windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
 puts windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+
+class MyQueue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(item)
+    @queue << item
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue[0]
+  end
+  
+  def size
+    @queue.length
+  end
+
+  def empty?
+    @queue.length == 0
+end
+
+class MyStack
+  def initialize
+    @stack = []
+  end
+
+  def push(item)
+    @stack.push(item)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last()
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.first == nil
+  end
+end
+
+class StackQueue
+  def initialize
+    @stack1 = MyStack.new()
+    @stack2 = Mystack.new()
+    @topstack = @stack1
+  end
+
+  def enqueue(item)
+    @topstack.push(item)
+    if @topstack == @stack1
+      @topstack = @stack2
+    else
+      @topstack = @stack1
+    end
+    dequeue
+  end
+
+  def dequeue
+    @topstack.pop(item)
+  end
+
+  def size
+    return @stack1 + @stack2
+  end
+
+  def empty?
+    return @stack1 + @stack2 == 0
+  end
+end

b5d9ec70a6b31edd59b70dac6485e37937df3d8d
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-11 19:01:25 -0500

    Complete phase 1

--- ruby/LRU_cache/lib/p01_int_set.rb
+++ ruby/LRU_cache/lib/p01_int_set.rb
@@ -1,19 +1,32 @@
 class MaxIntSet
+  attr_reader :store, :max
+
   def initialize(max)
+    raise ArgumentError.new("max must be an integer") unless max.is_a?(Integer)
+    raise ArgumentError.new("max must be greater than 0") if max <= 0
+    
+    @store = Array.new(max, false)
+    @max = max
   end
 
   def insert(num)
+    @store[num] = true if is_valid?(num)
+    return true
   end
 
   def remove(num)
+    @store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    @store[num] if is_valid?(num)
   end
 
   private
 
   def is_valid?(num)
+    raise ArgumentError.new("Out of bounds") if num >= @max or num < 0
+    num.is_a?(Integer)
   end
 
   def validate!(num)
@@ -27,18 +40,30 @@ class IntSet
   end
 
   def insert(num)
+    if not self[num].include?(num)
+      self[num] << num
+      return true
+    end
+    return false
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      return true
+    end
+    return false
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +80,36 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if @count == num_buckets
+      resize!
+    end
+
+    unless self[num].include?(num)
+      self[num] << num
+      @count += 1
+      return true
+    end
+    return false
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+      return true
+    end
+    return false
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +117,12 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |arr|
+      arr.each do |num|
+        new_store[num % (num_buckets * 2)] << num
+      end
+    end
+    @store = new_store
   end
 end

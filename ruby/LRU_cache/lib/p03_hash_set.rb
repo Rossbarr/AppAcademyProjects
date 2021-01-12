a2d7c8da903fce72210c922c137059979b476268
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-11 20:31:27 -0500

    Complete phase 3

--- ruby/LRU_cache/lib/p03_hash_set.rb
+++ ruby/LRU_cache/lib/p03_hash_set.rb
@@ -7,18 +7,41 @@ class HashSet
   end
 
   def insert(key)
+    if @count >= num_buckets
+      resize!
+    end
+
+    unless include?(key)
+      num = key.hash % num_buckets
+      self[num] << key
+      @count += 1
+      return true
+    end
+
+    return false
   end
 
   def include?(key)
+    num = key.hash % num_buckets
+    return self[num].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      num = key.hash % num_buckets
+      self[num].delete(key)
+      @count -= 1
+      return true
+    end
+
+    return false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 
   def num_buckets
@@ -26,5 +49,12 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |arr|
+      arr.each do |el|
+        new_store[el.hash % (num_buckets * 2)] << el
+      end
+    end
+    @store = new_store
   end
 end

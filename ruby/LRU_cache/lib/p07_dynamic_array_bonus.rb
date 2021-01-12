a60c22014dd07f7626015093fda609e4aa731249
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-12 14:40:41 -0500

    Complete phase 07; complete RUBY!

--- ruby/LRU_cache/lib/p07_dynamic_array_bonus.rb
+++ ruby/LRU_cache/lib/p07_dynamic_array_bonus.rb
@@ -22,11 +22,15 @@ class StaticArray
   private
 
   def validate!(i)
-    raise "Overflow error" unless i.between?(0, self.store.length - 1)
+    unless i.between?(0, self.store.length - 1)
+      raise "Overflow error"
+    end
   end
 end
 
 class DynamicArray
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(capacity = 8)
@@ -35,9 +39,25 @@ class DynamicArray
   end
 
   def [](i)
+    return nil if i > @count - 1
+    if i < 0 and i >= -@count
+      @store[@count + i]
+    elsif i >= 0
+      @store[i]
+    else
+      return nil
+    end
   end
 
   def []=(i, val)
+    return nil if i > @count
+    if i < 0 and i >= -@count
+      @store[@count + i] = val
+    elsif i >= 0
+      @store[i] = val
+    else
+      return nil
+    end
   end
 
   def capacity
@@ -45,27 +65,76 @@ class DynamicArray
   end
 
   def include?(val)
+    self.each do |el|
+      if val == el
+        return true
+      end
+    end
+    return false
   end
 
   def push(val)
+    if @count >= capacity
+      resize!
+    end
+
+    self[@count] = val
+    @count += 1
   end
 
   def unshift(val)
+    if @count >= capacity
+      resize!
+    end
+
+    i = 0
+    while 0 <= @count - i - 1
+      self[@count - i] = self[@count - i - 1]
+      i += 1
+    end
+
+    @store[0] = val
+    @count += 1
   end
 
   def pop
+    if @count > 0
+      node = last
+      @store[@count - 1] = nil
+      @count -= 1
+    else
+      node = nil
+    end
+    return node
   end
 
   def shift
+    val = self[0]
+    
+    i = 0
+    while i < @count
+      @store[i] = @store[i+1]
+      i += 1
+    end
+
+    @count -= 1
+    return val
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
   def each
+    i = 0
+    while i <= @count - 1
+      yield @store[i]
+      i += 1
+    end
   end
 
   def to_s
@@ -74,7 +143,14 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    i = 0
+    while i < @count - 1
+      if other[i] != self[i]
+        return false
+      end
+      i += 1
+    end
+    return true
   end
 
   alias_method :<<, :push
@@ -83,5 +159,12 @@ class DynamicArray
   private
 
   def resize!
+    new_array = StaticArray.new(@store.length * 2)
+    i = 0
+    self.each do |el|
+      new_array[i] = el
+      i += 1
+    end
+    @store = new_array
   end
 end

b5fd2adf697aa0b2008109dde7a1c891fc855762
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2021-01-11 20:02:46 -0500

    Finish Phase 2

--- ruby/LRU_cache/lib/p02_hashing.rb
+++ ruby/LRU_cache/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    placeholder_string = ""
+    self.each_with_index do |num, i|
+      placeholder_string += num.to_s + i.to_s
+    end
+    return placeholder_string.to_i.hash
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a
+    placeholder_int = 0
+    self.each_char.with_index do |char, i|
+      placeholder_int += alphabet.index(char.downcase) * i
+    end
+    return placeholder_int.hash
   end
 end
 
@@ -16,6 +27,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    placeholder_int = 0
+    array = self.to_a.sort!
+    array.each do |arr|
+      placeholder_int += arr.hash
+    end
+    placeholder_int.hash
   end
 end

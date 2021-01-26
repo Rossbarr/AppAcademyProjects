84fffcf21594c247c3bbd74475c99e21e245aa2d
Rossbarr <Rossbarr@msu.edu>
2021-01-26 15:27:19 -0500

    Complete associations exercise

--- sql/associations_excercise/app/models/course.rb
+++ sql/associations_excercise/app/models/course.rb
@@ -1,2 +1,24 @@
 class Course < ApplicationRecord
+    has_many(:enrollments, {
+        class_name: 'Enrollment',
+        foreign_key: :course_id,
+        primary_key: :id
+    })
+
+    has_many(:enrolled_students, {
+        through: :enrollments,
+        source: :student
+    })
+
+    belongs_to(:prerequisite, {
+        class_name: 'Course',
+        foreign_key: :prereq_id,
+        primary_key: :id
+    })
+
+    belongs_to(:instructor, {
+        class_name: 'User',
+        foreign_key: :instructor_id,
+        primary_key: :id
+    })
 end
--- sql/associations_excercise/app/models/enrollment.rb
+++ sql/associations_excercise/app/models/enrollment.rb
@@ -1,2 +1,13 @@
 class Enrollment < ApplicationRecord
+    belongs_to(:course, {
+        class_name: 'Course',
+        foreign_key: :course_id,
+        primary_key: :id
+    })
+
+    belongs_to(:student, {
+        class_name: 'User',
+        foreign_key: :student_id,
+        primary_key: :id
+    })
 end

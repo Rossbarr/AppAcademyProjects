90a6d24bdf80c2601889ff21660ecf66ed96d7d5
Rossbarr <Rossbarr@msu.edu>
2021-02-01 15:36:07 -0500

    Complete through to phase 1d

--- sql/build_your_own_active_record/lib/00_attr_accessor_object.rb
+++ sql/build_your_own_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      name = name.to_s
+
+      define_method(name) do
+        instance_variable_get("@" + name)
+      end
+
+      define_method(name + "=") do |new_value|
+        instance_variable_set("@" + name, new_value)
+      end
+    end
   end
 end
--- sql/build_your_own_active_record/lib/01_sql_object.rb
+++ sql/build_your_own_active_record/lib/01_sql_object.rb
@@ -5,18 +5,39 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    unless @columns #check if columns already exists, if it doesn't...
+      result = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+      SQL
+      @columns = result[0].map(&:to_sym)
+    else # if columns does exist...
+      @columns
+    end
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column.to_s) do
+        self.attributes[column]
+      end
+
+      define_method(column.to_s + "=") do |new_value|
+        self.attributes[column] = new_value
+      end
+    end
+
+    @attributes
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.name.tableize
   end
 
   def self.all
@@ -32,11 +53,17 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise ArgumentError.new("unknown attribute '#{attr_name}'")
+      else
+        self.send(attr_name.to_s + "=", value)
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values

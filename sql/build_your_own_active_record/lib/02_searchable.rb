cccff9c7f0e731f5114f253c3e5833739422d186
Rossbarr <Rossbarr@msu.edu>
2021-02-02 12:07:17 -0500

    Complete phase 2

--- sql/build_your_own_active_record/lib/02_searchable.rb
+++ sql/build_your_own_active_record/lib/02_searchable.rb
@@ -3,10 +3,22 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |attr_name| "#{attr_name} = ?" }.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end

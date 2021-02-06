2eff8fe22652c39e1a6ff45435803daf7308efd1
Rossbarr <Rossbarr@msu.edu>
2021-02-05 19:48:43 -0500

    Update users_controller with primitive actions

--- rails/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def index
+        render(json: params)
+    end
+
+    def create
+        render(json: params)
+    end
+
+    def show
+        render(json: params)
+    end
 end

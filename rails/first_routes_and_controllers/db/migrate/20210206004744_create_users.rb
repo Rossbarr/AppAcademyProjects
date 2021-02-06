7576f5fd5612a193b58ae3f3fe141db31ea64e7f
Rossbarr <Rossbarr@msu.edu>
2021-02-05 19:57:51 -0500

    Finalize migration for users

--- rails/first_routes_and_controllers/db/migrate/20210206004744_create_users.rb
+++ rails/first_routes_and_controllers/db/migrate/20210206004744_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string(:name, null: false)
+      t.string(:email, null: false)
+
+      t.index(:name)
+
+      t.timestamps()
     end
   end
 end

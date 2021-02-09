3cce09cfba7d6654dd340ee5a544a5d6c0cac833
rossbarr <rossbarr@msu.edu>
2021-02-09 13:39:08 -0500

    Create artwork shares controller

--- rails/first_routes_and_controllers/config/routes.rb
+++ rails/first_routes_and_controllers/config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    only: [:index, :show, :create, :update, :destroy]
-  end
-  # This defines:
-  # get 'users',          to: 'users#index',    as: 'users'
-  # post 'users',         to: 'users#create'
-  # get 'users/:id',      to: 'users#show',     as: 'user'
-  # patch 'users/:id',    to: 'users#update'
-  # put 'users/:id',      to: 'users#update'
-  # delete 'users/:id',   to: 'users#destroy'
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  resources :artworks, only: [:index, :show, :create, :update, :destroy]
+
+  resources :artwork_shares, only: [:create, :destroy]
 end

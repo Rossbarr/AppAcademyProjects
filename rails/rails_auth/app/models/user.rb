a6036c9f0def933dd2ecd02a368d22982dac4083
rossbarr <rossbarr@msu.edu>
2021-03-08 15:12:30 -0500

    Create User model

--- rails/rails_auth/app/models/user.rb
+++ rails/rails_auth/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates [:username, :session_token], presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "password can't be blank" }
+  validates :password, length: { minimum: 8 allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  def ensure_session_token
+    @session_token ||= SecureRandom.urlsafe_base64
+  end
 end

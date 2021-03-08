8617fafa91136a4cb367126fb6cd930b0fbc368c
rossbarr <rossbarr@msu.edu>
2021-03-08 16:41:23 -0500

    Create Users table

--- rails/cats99/db/schema.rb
+++ rails/cats99/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_220925) do
+ActiveRecord::Schema.define(version: 2021_03_08_213754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2021_02_25_220925) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", limit: 30, null: false
+    t.string "password_digest", null: false
+    t.string "session_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "cat_rental_requests", "cats", on_delete: :cascade

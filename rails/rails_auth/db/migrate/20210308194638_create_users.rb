f23344101b83f7eea7b846e4f8ba12b864034df0
rossbarr <rossbarr@msu.edu>
2021-03-08 15:01:50 -0500

    Create Users table

--- rails/rails_auth/db/migrate/20210308194638_create_users.rb
+++ rails/rails_auth/db/migrate/20210308194638_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-      t.string :session_token
+      t.string :username, null: false, limit: 30
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, default: nil
 
       t.timestamps
     end
+
+    add_index(:users, :username, unique: true)
   end
 end

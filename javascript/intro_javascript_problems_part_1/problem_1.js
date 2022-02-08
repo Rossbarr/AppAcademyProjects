f02c79ac8b04945310eac4e90ceb685c05d7472c
rossbarr <barrettross23@gmail.com>
2022-02-08 13:50:31 -0500

    Finish problem 1

--- javascript/intro_javascript_problems_part_1/problem_1.js
+++ javascript/intro_javascript_problems_part_1/problem_1.js
@@ -16,14 +16,14 @@ function mysteryScoping2() {
   console.log(x);
 }
 
-function mysteryScoping3() {
-  const x = 'out of block';
-  if (true) {
-    var x = 'in block';  
-    console.log(x);
-  }
-  console.log(x);
-}
+// function mysteryScoping3() {
+//   const x = 'out of block';
+//   if (true) {
+//     var x = 'in block';  
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
 
 function mysteryScoping4() {
   let x = 'out of block';
@@ -34,12 +34,18 @@ function mysteryScoping4() {
   console.log(x);
 }
 
-function mysteryScoping5() {
-  let x = 'out of block';
-  if (true) {
-    let x = 'in block';  
-    console.log(x);
-  }
-  let x = 'out of block again';
-  console.log(x);
-}
+// function mysteryScoping5() {
+//   let x = 'out of block';
+//   if (true) {
+//     let x = 'in block';  
+//     console.log(x);
+//   }
+//   let x = 'out of block again';
+//   console.log(x);
+// }
+
+// mysteryScoping1(); // "in block"; "in block"
+// mysteryScoping2(); // 'in block'; 'out of block'
+// mysteryScoping3(); // Throws an error at definition; x has already been declared
+// mysteryScoping4(); // 'in block'; 'out of block'
+// mysteryScoping5(); // Throws an error at definition; x has already been declared

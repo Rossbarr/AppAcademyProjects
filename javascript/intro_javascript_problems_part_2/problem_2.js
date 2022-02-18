e3e197d271f659cc3e017f684691f4e1c6c21684
rossbarr <barrettross23@gmail.com>
2022-02-18 12:38:51 -0500

    Finish problem 3

--- javascript/intro_javascript_problems_part_2/problem_2.js
+++ javascript/intro_javascript_problems_part_2/problem_2.js
@@ -24,13 +24,28 @@ Elephant.prototype.play = function () {
   console.log(this.name, "is ", this.tricks[getRandomInt(this.tricks.length)]);
 }
 
-ele = new Elephant('Salgat', 12, ['jumping'])
-console.log(ele.name)
-console.log(ele.height)
-ele.play()
-ele.grow()
-console.log(ele.height)
-ele.addTrick('eating')
-ele.play()
-ele.play()
-ele.play()
+// ele = new Elephant('Salgat', 12, ['jumping'])
+// console.log(ele.name)
+// console.log(ele.height)
+// ele.play()
+// ele.grow()
+// console.log(ele.height)
+// ele.addTrick('eating')
+// ele.play()
+// ele.play()
+// ele.play()
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+function paradeHelper (elephant) {
+  console.log(elephant.name, 'is trotting by!');
+}
+
+herd.forEach(element => {
+  paradeHelper(element);
+});

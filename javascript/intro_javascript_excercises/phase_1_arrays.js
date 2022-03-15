43c7465df3857831f8f3b8bec0c5341e892f0172
rossbarr <barrettross23@gmail.com>
2022-03-14 23:21:48 -0400

    More progress. Need to remember how hashmaps work

--- javascript/intro_javascript_excercises/phase_1_arrays.js
+++ javascript/intro_javascript_excercises/phase_1_arrays.js
@@ -12,10 +12,15 @@ Array.prototype.uniq = function() {
 
 // console.log([1,2,2,3,3,3].uniq());
 
-Array.prototype.twoSum = function() {
+// https://www.youtube.com/watch?v=KLlXCFG5TnA ""
+Array.prototype.twoSum = function(target = 0) {
+  // Check if the value to add is already in the hashmap
+
   hash = new Map();
+  let i = 0;
 
-  while (true) {
-
-  }
+  do {
+    hash.set(this[i], i)
+    if (target - this[i])
+  } while (i < this.length);
 }

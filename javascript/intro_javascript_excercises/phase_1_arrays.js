c225203f7d3dd31076f26a4bdc243a090df42722
rossbarr <barrettross23@gmail.com>
2022-03-22 19:35:53 -0400

    incremental progress

--- javascript/intro_javascript_excercises/phase_1_arrays.js
+++ javascript/intro_javascript_excercises/phase_1_arrays.js
@@ -12,15 +12,42 @@ Array.prototype.uniq = function() {
 
 // console.log([1,2,2,3,3,3].uniq());
 
-// https://www.youtube.com/watch?v=KLlXCFG5TnA ""
 Array.prototype.twoSum = function(target = 0) {
-  // Check if the value to add is already in the hashmap
+  const num_pos = {};
 
-  hash = new Map();
   let i = 0;
 
   do {
-    hash.set(this[i], i)
-    if (target - this[i])
+    num_pos[this[i]] = i;
+    required_num_pos = num_pos[target - this[i]];
+    if (required_num_pos != undefined && required_num_pos != i) {
+      return [required_num_pos, i]
+    }
+    i++;
   } while (i < this.length);
 }
+
+// console.log([2,3,4,6,5,6].twoSum(9))
+
+Array.prototype.transpose = function() {
+  let max = -Infinity;
+  let index = -1;
+  this.forEach(function(a, i){
+    if (a.length > max) {
+      max = a.length;
+      index = i;
+    }
+  });
+
+  // Create an array of transposed size filled with null
+  const new_array = Array(max).fill(Array(this.length).fill(null))
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this[i].length; j++) {
+      new_array[j].push(this[i][j]);
+    }
+  }
+  return new_array;
+}
+
+console.log([[1,2,3,4],[3,2],[2],[1]].transpose());
